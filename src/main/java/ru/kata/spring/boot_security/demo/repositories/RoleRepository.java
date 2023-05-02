91f596c0aa32ac2590cbae97bfd4e31ac501ba21
Toogoolka <120179327+Toogoolka@users.noreply.github.com>
2023-05-02 16:50:30 +0300

    delete @repo

--- src/main/java/ru/kata/spring/boot_security/demo/repositories/RoleRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repositories/RoleRepository.java
@@ -6,7 +6,6 @@ import ru.kata.spring.boot_security.demo.model.Role;
 
 import java.util.List;
 
-@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     public List<Role> findAll();
     public Role findRoleByName(String name);
